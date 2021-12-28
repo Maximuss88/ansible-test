172287186ae5a864cb1af490ef33642459b87dfc
Максим Плаксин <maksim878@rambler.ru>
2021-12-28 20:25:16 +0400

    add new playbook

--- 1.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker start b6d31332898c 6d43adf88ad5 32b286339191
-ansible-playbook -i inventory/prod.yml site.yml --ask-vault-pass
-docker stop b6d31332898c 6d43adf88ad5 32b286339191
